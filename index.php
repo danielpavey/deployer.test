6589607bcfd9d0931352b76ca2a6af31f79f1201
danielpavey <daniel.pavey@axminster.co.uk>
2017-04-20 08:32:23 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>   
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Test</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="" />
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css?v=1">
+</head>
+<body>
+    <header>
+        <h1>This is a Test</h1>
+        </header>
+        
+        <section id="main">
+
+        </section>
+        
+        <footer>
+        
+        </footer>
+        </body>
+</html>
